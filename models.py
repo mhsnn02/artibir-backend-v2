2ce65d4fc56cc7f29e460081cbce27cbe0250d45
mhsnn02 <mhsntanr1verd1x@gmail.com>
2026-01-29 00:41:13 +0300

    fix: removed redundant index from User primary key

--- models.py
+++ models.py
@@ -36,7 +36,7 @@ class PaymentStatus(enum.Enum):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     email = Column(String, unique=True, index=True)
     password = Column(String)
     full_name = Column(String)
